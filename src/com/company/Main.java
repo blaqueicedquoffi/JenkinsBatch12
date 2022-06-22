e51d4f735ad7467a21ff634d9a1c32b98f9c957a
blaquevybes <blaquevybes@gmail.com>
2022-06-22 18:31:10 -0500

    My first Jenkins project

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write code here
+        for (int i=0 ; i<5; i++ ) {
+            System.out.println("Hello! I'm Manny");
+        }
+    }
+}
